057fad33e932f34a50788a5a4c6057ff4776026b
Muhammad Fajar <invasionfajar@gmail.com>
2021-06-17 19:22:45 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import { SplashScreen } from './pages';
+
+const App = () => {
+  return (
+    <View>
+      <SplashScreen />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
